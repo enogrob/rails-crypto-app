ddf16181d79b2a4cc0eea67d133b3b695eb72bcd
Roberto Nogueira <enogrob@gmail.com>
2018-05-21 20:23:34 -0300

    add-lookup-action-into-home-controller

--- spec/controllers/home_controller_spec.rb
+++ spec/controllers/home_controller_spec.rb
@@ -1,4 +1,6 @@
 require 'rails_helper'
+require 'net/http'
+require 'json'
 
 RSpec.describe HomeController, type: :controller do
 
@@ -12,6 +14,20 @@ RSpec.describe HomeController, type: :controller do
   describe "GET #about" do
     it "returns http success" do
       get :about
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe "GET #lookup" do
+    it "returns http success" do
+      get :lookup
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe "POST #lookup" do
+    it "returns http success" do
+      post :lookup
       expect(response).to have_http_status(:success)
     end
   end
